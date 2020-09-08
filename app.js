d2d3a0125ed4d3e6c276ae4f33596bdf2f57053c
j30ng <leej0116@outlook.com>
2020-09-09 03:44:41 +0900

    Update app.js
    Express apparently needs a default engine

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
